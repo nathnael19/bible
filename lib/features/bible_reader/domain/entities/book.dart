24300ee8ecb5ac60ef3c8aacef3b2c14dc1ecf29
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2026-03-20 13:36:45 +0300

    Add englishName property to Book entity for enhanced data representation

--- lib/features/bible_reader/domain/entities/book.dart
+++ lib/features/bible_reader/domain/entities/book.dart
@@ -3,14 +3,16 @@ import 'package:equatable/equatable.dart';
 class Book extends Equatable {
   final String id;
   final String name;
+  final String englishName;
   final int chapterCount;
 
   const Book({
     required this.id,
     required this.name,
+    required this.englishName,
     required this.chapterCount,
   });
 
   @override
-  List<Object?> get props => [id, name, chapterCount];
+  List<Object?> get props => [id, name, englishName, chapterCount];
 }
