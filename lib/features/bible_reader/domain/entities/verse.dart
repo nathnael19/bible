2ad656c652cb2f72f6ce5ab7ddd2c6e38099a7c4
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2026-03-20 14:09:37 +0300

    Update Verse class to include bookId in props for improved equality comparison

--- lib/features/bible_reader/domain/entities/verse.dart
+++ lib/features/bible_reader/domain/entities/verse.dart
@@ -4,15 +4,17 @@ class Verse extends Equatable {
   final int number;
   final String text;
   final String bookName;
+  final String bookId;
   final int chapter;
 
   const Verse({
     required this.number,
     required this.text,
     required this.bookName,
+    required this.bookId,
     required this.chapter,
   });
 
   @override
-  List<Object?> get props => [number, text, bookName, chapter];
+  List<Object?> get props => [number, text, bookName, bookId, chapter];
 }
