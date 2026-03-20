455d592de0ddcbc01d336b6ec3c9d5c02b62144d
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2026-03-20 16:14:06 +0300

    Refactor ThemeCubit to utilize LocalStorage for theme management. Update toggleTheme method to streamline theme switching and ensure persistence of user preferences.

--- lib/features/bible_reader/presentation/cubit/theme_cubit.dart
+++ lib/features/bible_reader/presentation/cubit/theme_cubit.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:bible/core/services/local_storage.dart';
 
 class ThemeCubit extends Cubit<ThemeMode> {
-  ThemeCubit() : super(ThemeMode.light);
+  final LocalStorage _storage;
+
+  ThemeCubit(this._storage) : super(_storage.getThemeMode());
 
   void toggleTheme() {
-    if (state == ThemeMode.light) {
-      emit(ThemeMode.dark);
-    } else {
-      emit(ThemeMode.light);
-    }
+    final newMode = state == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
+    setTheme(newMode);
   }
 
   void setTheme(ThemeMode mode) {
+    _storage.setThemeMode(mode);
     emit(mode);
   }
 }
