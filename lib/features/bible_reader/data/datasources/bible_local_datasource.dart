95dfb164f8c92ce77337bebb3b7f650ff9b411be
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2026-03-20 13:36:40 +0300

    Refactor getBooks method in BibleLocalDataSource to include English book names
    This commit enhances the getBooks method by iterating through the cached Bible data and adding English names for each book based on their IDs. A new private method, _getEnglishName, is introduced to map book IDs to their corresponding English names, improving the data representation in the application.

--- lib/features/bible_reader/data/datasources/bible_local_datasource.dart
+++ lib/features/bible_reader/data/datasources/bible_local_datasource.dart
@@ -32,7 +32,87 @@ class BibleLocalDataSource {
 
   Future<List<BookModel>> getBooks() async {
     await _init();
-    return _cachedBible?.map((b) => BookModel.fromJson(b)).toList() ?? [];
+    final List<BookModel> books = [];
+    if (_cachedBible != null) {
+      for (final b in _cachedBible!) {
+        final id = b['book_id'] as int;
+        final englishName = _getEnglishName(id);
+        books.add(BookModel.fromJson(b, englishName));
+      }
+    }
+    return books;
+  }
+
+  String _getEnglishName(int id) {
+    const names = {
+      1: 'GENESIS',
+      2: 'EXODUS',
+      3: 'LEVITICUS',
+      4: 'NUMBERS',
+      5: 'DEUTERONOMY',
+      6: 'JOSHUA',
+      7: 'JUDGES',
+      8: 'RUTH',
+      9: '1 SAMUEL',
+      10: '2 SAMUEL',
+      11: '1 KINGS',
+      12: '2 KINGS',
+      13: '1 CHRONICLES',
+      14: '2 CHRONICLES',
+      15: 'EZRA',
+      16: 'NEHEMIAH',
+      17: 'ESTHER',
+      18: 'JOB',
+      19: 'PSALMS',
+      20: 'PROVERBS',
+      21: 'ECCLESIASTES',
+      22: 'SONG OF SOLOMON',
+      23: 'ISAIAH',
+      24: 'JEREMIAH',
+      25: 'LAMENTATIONS',
+      26: 'EZEKIEL',
+      27: 'DANIEL',
+      28: 'HOSEA',
+      29: 'JOEL',
+      30: 'AMOS',
+      31: 'OBADIAH',
+      32: 'JONAH',
+      33: 'MICAH',
+      34: 'NAHUM',
+      35: 'HABAKKUK',
+      36: 'ZEPHANIAH',
+      37: 'HAGGAI',
+      38: 'ZECHARIAH',
+      39: 'MALACHI',
+      40: 'MATTHEW',
+      41: 'MARK',
+      42: 'LUKE',
+      43: 'JOHN',
+      44: 'ACTS',
+      45: 'ROMANS',
+      46: '1 CORINTHIANS',
+      47: '2 CORINTHIANS',
+      48: 'GALATIANS',
+      49: 'EPHESIANS',
+      50: 'PHILIPPIANS',
+      51: 'COLOSSIANS',
+      52: '1 THESSALONIANS',
+      53: '2 THESSALONIANS',
+      54: '1 TIMOTHY',
+      55: '2 TIMOTHY',
+      56: 'TITUS',
+      57: 'PHILEMON',
+      58: 'HEBREWS',
+      59: 'JAMES',
+      60: '1 PETER',
+      61: '2 PETER',
+      62: '1 JOHN',
+      63: '2 JOHN',
+      64: '3 JOHN',
+      65: 'JUDE',
+      66: 'REVELATION',
+    };
+    return names[id] ?? 'BOOK $id';
   }
 
   Future<int> getChapterCount(String book) async {
