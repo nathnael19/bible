df82632066e3a9728e3d6abb8512046f76da961f
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2026-03-20 11:39:15 +0300

    Enhance IBibleRepository with methods for retrieving books and chapter counts
    This commit adds the getBooks and getChapterCount methods to the IBibleRepository interface, enabling the retrieval of Bible books and the count of chapters for a specified book. These additions support improved data management and functionality within the BibleApp.

--- lib/features/bible_reader/domain/repositories/i_bible_repository.dart
+++ lib/features/bible_reader/domain/repositories/i_bible_repository.dart
@@ -1,8 +1,11 @@
 import '../entities/bible_version.dart';
 import '../entities/verse.dart';
+import '../entities/book.dart';
 
 abstract class IBibleRepository {
   Future<List<BibleVersion>> getBibleVersions();
+  Future<List<Book>> getBooks();
+  Future<int> getChapterCount(String book);
   Future<List<Verse>> getVerses({
     required String versionId,
     required String book,
