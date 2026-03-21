03bdee9bca1aa5c3ad59a1a1afc952ced60105f3
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2026-03-21 21:30:44 +0300

    Add OnboardingCubit to manage onboarding state, utilizing SharedPreferences to track completion status. This enhances the onboarding flow by allowing the app to remember if the user has completed onboarding.

---
+++ lib/features/onboarding/presentation/cubit/onboarding_cubit.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class OnboardingCubit extends Cubit<bool> {
+  final SharedPreferences _prefs;
+  static const String _onboardingCompleteKey = 'onboarding_complete';
+
+  OnboardingCubit(this._prefs) : super(_prefs.getBool(_onboardingCompleteKey) ?? false);
+
+  Future<void> completeOnboarding() async {
+    await _prefs.setBool(_onboardingCompleteKey, true);
+    emit(true);
+  }
+}
