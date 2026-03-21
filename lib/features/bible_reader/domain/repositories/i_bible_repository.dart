e03cc0d97ab086f1de7e7b4da4c8197a5c87bc58
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2026-03-22 01:06:19 +0300

    Refactor IBibleRepository to include versionId parameter in getBooks and searchVerses methods for improved flexibility
    This commit updates the IBibleRepository interface to accept an optional versionId parameter in the getBooks and searchVerses methods, defaulting to 'amh_standard'. This enhancement aligns with recent changes in the repository, allowing for more versatile data retrieval based on different Bible versions.

--- lib/features/bible_reader/domain/repositories/i_bible_repository.dart
+++ lib/features/bible_reader/domain/repositories/i_bible_repository.dart
@@ -5,12 +5,15 @@ import '../entities/search_filter.dart';
 
 abstract class IBibleRepository {
   Future<List<BibleVersion>> getBibleVersions();
-  Future<List<Book>> getBooks();
-  Future<int> getChapterCount(String book);
+  Future<List<Book>> getBooks({String versionId = 'amh_standard'});
+  Future<int> getChapterCount(String book, {String versionId = 'amh_standard'});
   Future<List<Verse>> getVerses({
     required String versionId,
     required String book,
     required int chapter,
   });
-  Future<List<Verse>> searchVerses(String query, {SearchFilter filter = SearchFilter.all});
+  Future<List<Verse>> searchVerses(String query, {
+    String versionId = 'amh_standard',
+    SearchFilter filter = SearchFilter.all,
+  });
 }
