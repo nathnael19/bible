feeed3ddca1f0e81572ee25d80ae124872b0c01d
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2026-03-21 21:58:58 +0300

    Refactor OnboardingCubit constructor for improved code readability. Adjusted formatting for better structure and clarity in the initialization of onboarding completion state.

--- lib/features/onboarding/presentation/cubit/onboarding_cubit.dart
+++ lib/features/onboarding/presentation/cubit/onboarding_cubit.dart
@@ -5,7 +5,8 @@ class OnboardingCubit extends Cubit<bool> {
   final SharedPreferences _prefs;
   static const String _onboardingCompleteKey = 'onboarding_complete';
 
-  OnboardingCubit(this._prefs) : super(_prefs.getBool(_onboardingCompleteKey) ?? false);
+  OnboardingCubit(this._prefs)
+    : super(_prefs.getBool(_onboardingCompleteKey) ?? false);
 
   Future<void> completeOnboarding() async {
     await _prefs.setBool(_onboardingCompleteKey, true);
