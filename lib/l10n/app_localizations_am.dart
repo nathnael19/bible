e6370e236964274e1fe5e78bf7e7c57ed103b752
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2026-03-21 21:21:48 +0300

    Add Amharic localization support in AppLocalizationsAm, enhancing accessibility for Amharic speakers.

---
+++ lib/l10n/app_localizations_am.dart
@@ -0,0 +1,369 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Amharic (`am`).
+class AppLocalizationsAm extends AppLocalizations {
+  AppLocalizationsAm([String locale = 'am']) : super(locale);
+
+  @override
+  String get appTitle => 'ቅዱስ መጽሐፍ';
+
+  @override
+  String get home => 'መነሻ';
+
+  @override
+  String get reading => 'ንባብ';
+
+  @override
+  String get profile => 'መገለጫ';
+
+  @override
+  String get language => 'ቋንቋ';
+
+  @override
+  String get amharic => 'አማርኛ';
+
+  @override
+  String get english => 'እንግሊዝኛ';
+
+  @override
+  String get settings => 'ቅንብሮች';
+
+  @override
+  String get logout => 'ውጣ';
+
+  @override
+  String get lastRead => 'ለመጨረሻ ጊዜ የተነበበ';
+
+  @override
+  String get versesRead => 'የተነበቡ ጥቅሶች';
+
+  @override
+  String get bookmarks => 'ምልክቶች';
+
+  @override
+  String get streak => 'የንባብ ቀናት';
+
+  @override
+  String get chapter => 'ምዕራፍ';
+
+  @override
+  String get myArchives => 'የእኔ ማህደሮች';
+
+  @override
+  String get bookmarkedChapters => 'ምልክት የተደረገባቸው ምዕራፎች';
+
+  @override
+  String get notReadYet => 'ገና አልተነበበም';
+
+  @override
+  String get amharicStandard => 'አማርኛ መደበኛ';
+
+  @override
+  String get kingJames => 'ኪንግ ጄምስ ቨርዥን';
+
+  @override
+  String get basicEnglish => 'መሰረታዊ እንግሊዝኛ';
+
+  @override
+  String get welcomeBack => 'እንኳን ደህና መጡ';
+
+  @override
+  String get loginSubtitle => 'መንፈሳዊ ጉዞዎን በመለያዎ ይጀምሩ።';
+
+  @override
+  String get usernameOrEmail => 'የተጠቃሚ ስም ወይም ኢሜይል';
+
+  @override
+  String get password => 'የይለፍ ቃል';
+
+  @override
+  String get forgotPassword => 'የይለፍ ቃልዎን ረስተዋል?';
+
+  @override
+  String get login => 'ግባ';
+
+  @override
+  String get or => 'ወይም';
+
+  @override
+  String get noAccount => 'መለያ የለዎትም?';
+
+  @override
+  String get createAccount => 'መለያ ይፍጠሩ';
+
+  @override
+  String get createAccountTitle => 'መለያ ይፍጠሩ';
+
+  @override
+  String get registerSubtitle => 'ጉዞዎን ከእኛ ጋር ይጀምሩ እና ቅዱሳት መጻሕፍትን ይመርምሩ።';
+
+  @override
+  String get fullName => 'ሙሉ ስም';
+
+  @override
+  String get fullNameHint => 'ዮሐንስ አበበ';
+
+  @override
+  String get email => 'ኢሜይል';
+
+  @override
+  String get emailHint => 'email@example.com';
+
+  @override
+  String get confirmPassword => 'የይለፍ ቃል ያረጋግጡ';
+
+  @override
+  String get registerTermsPrefix => 'በመመዝገብዎ በ ';
+
+  @override
+  String get termsAndConditions => 'አጠቃቀም ደንቦች';
+
+  @override
+  String get registerTermsAnd => ' እና ';
+
+  @override
+  String get privacyPolicy => 'ግላዊነት ፖሊሲ';
+
+  @override
+  String get registerTermsSuffix => ' ይስማማሉ።';
+
+  @override
+  String get passwordsDontMatch => 'የይለፍ ቃሎቹ አይዛመዱም';
+
+  @override
+  String get registerNow => 'አሁኑኑ ይመዝገቡ';
+
+  @override
+  String get alreadyHaveAccount => 'መለያ አለዎት?';
+
+  @override
+  String get loginLink => 'ግባ';
+
+  @override
+  String get forgotPasswordTitle => 'የይለፍ ቃልዎን ይርሱ?';
+
+  @override
+  String get forgotPasswordSubtitle =>
+      'የReset ኮድ ለመቀበል ከመለያዎ ጋር የተያያዘውን ኢሜይል ያስገቡ።';
+
+  @override
+  String get sendCode => 'ኮድ ላክ';
+
+  @override
+  String get backToLogin => 'ወደ መግቢያ ተመለስ';
+
+  @override
+  String get checkEmailTitle => 'ኢሜይልዎን ያረጋግጡ';
+
+  @override
+  String get checkEmailSubtitle1 => 'ባለ 4-አሃዝ ኮድ ወደ ';
+
+  @override
+  String get checkEmailSubtitle2 => ' ልከናል፡፡ እባክዎን ከታች ያስገቡ።';
+
+  @override
+  String get errorOccurred => 'ችግር ተከስቷል';
+
+  @override
+  String get passwordChangedSuccess => 'የይለፍ ቃል በትክክል ተቀይሯል!';
+
+  @override
+  String get verify => 'አረጋግጥ';
+
+  @override
+  String get didNotReceiveCode => 'ኮዱ አልደረሰዎትም? ';
+
+  @override
+  String get codeResent => 'ኮዱ በድጋሚ ተልኳል';
+
+  @override
+  String get resendCode => 'በድጋሚ ላክ';
+
+  @override
+  String get resend => 'በድጋሚ ላክ';
+
+  @override
+  String get continueReading => 'ንባብዎን ይቀጥሉ';
+
+  @override
+  String get seeAll => 'ሁሉንም ይመልከቱ';
+
+  @override
+  String get completed => 'ተጠናቋል';
+
+  @override
+  String get readingPlans => 'የንባብ ዕቅዶች';
+
+  @override
+  String get recentActivity => 'የቅርብ ጊዜ እንቅስቃሴ';
+
+  @override
+  String get dailyVerse => 'የዕለቱ ጥቅስ';
+
+  @override
+  String get peacePathPlan => 'የሰላም መንገድ';
+
+  @override
+  String get peacePathDesc => 'በዕለታዊ ጊዜ የሚደረጉ መጽሐፍ ቅዱሳዊ ምክሮችና ጸሎቶች ለ፲፭ ቀናት።';
+
+  @override
+  String get fastingSeasonPlan => 'የጾም ወቅት';
+
+  @override
+  String get fastingSeasonDesc => 'ለአርባ ቀን የሚቆይ የንባብ ዕቅድ';
+
+  @override
+  String get newPlan => 'አዲስ ዕቅድ';
+
+  @override
+  String get recordedVerse => 'ጥቅስ መዝግበዋል';
+
+  @override
+  String get finishedChapter => 'ምዕራፍ አጠናቀዋል';
+
+  @override
+  String get twoHoursAgo => 'ከ2 ሰዓት በፊት';
+
+  @override
+  String get yesterday => 'ትናንት';
+
+  @override
+  String get oldTestament => 'ብሉይ ኪዳን';
+
+  @override
+  String get newTestament => 'ሐዲስ ኪዳን';
+
+  @override
+  String get oldTestamentBooks => 'የብሉይ ኪዳን መጽሐፍት';
+
+  @override
+  String get newTestamentBooks => 'የሐዲስ ኪዳን መጽሐፍት';
+
+  @override
+  String get books => 'መጽሐፍት';
+
+  @override
+  String get search => 'ፍለጋ';
+
+  @override
+  String get searchHint => 'ይፈልጉ...';
+
+  @override
+  String get copy => 'ቅዳ';
+
+  @override
+  String get share => 'አጋራ';
+
+  @override
+  String get fontSize => 'የፊደል መጠን';
+
+  @override
+  String get colorTheme => 'የቀለም ጭብጥ';
+
+  @override
+  String get fontSettings => 'የፊደል ቅንጅቶች';
+
+  @override
+  String get themeSettings => 'የገጽታ ቅንጅቶች';
+
+  @override
+  String get read => 'አንብብ';
+
+  @override
+  String get dailyReflection => 'የዕለቱ አስተንትኖ';
+
+  @override
+  String get record => 'መዝግብ';
+
+  @override
+  String get close => 'ዝጋ';
+
+  @override
+  String get bibleVersions => 'የመጽሐፍ ቅዱስ ትርጉሞች';
+
+  @override
+  String get noResults => 'ምንም ውጤት አልተገኘም';
+
+  @override
+  String get resultsFound => 'ውጤቶች ተገኝተዋል';
+
+  @override
+  String get layout => 'እይታ';
+
+  @override
+  String get showMore => 'ተጨማሪ ውጤቶች አሳይ';
+
+  @override
+  String get featured => 'ተለይተው የቀረቡ';
+
+  @override
+  String get nowPlaying => 'አሁን በመጫወት ላይ';
+
+  @override
+  String get gospelDescriptor => 'የጌታችን የኢየሱስ ክርስቶስ ወንጌል';
+
+  @override
+  String get nextChapters => 'የሚቀጥሉት ምዕራፎች';
+
+  @override
+  String get chapterOne => 'ምዕራፍ አንድ';
+
+  @override
+  String get chapterTwo => 'ምዕራፍ ሁለት';
+
+  @override
+  String get chapterThree => 'ምዕራፍ ሶስት';
+
+  @override
+  String get chapterFour => 'ምዕራፍ አራት';
+
+  @override
+  String get sleep => 'እንቅልፍ';
+
+  @override
+  String get johnGospel => 'የዮሐንስ ወንጌል';
+
+  @override
+  String get noDataFound => 'ምንም መረጃ አልተገኘም።';
+
+  @override
+  String get compareVersions => 'ማነጻጸሪያ';
+
+  @override
+  String get selectVersion => 'ትርጉም ይምረጡ';
+
+  @override
+  String get comparisonNoteTitle => 'የንጽጽር ማስታወሻ';
+
+  @override
+  String get comparisonNoteBody =>
+      'በዚህ ሁኔታ ቃላት ትርጉሞች መካከል \"ቃል\" (Logos) የሚለው ቃል አጠቃቀም ተመሳሳይ ቢሆንም በአረፍተ ነገር በኩታ ሰንሰለት በያዘው መንገድ በአዲሱ መደበኛ ትርጉም ተቀይሮዋል።';
+
+  @override
+  String get selectChapter => 'ምዕራፍ ይምረጡ';
+
+  @override
+  String get selectVerse => 'ቁጥር ይምረጡ';
+
+  @override
+  String get tapToChange => 'ምዕራፍ ወይም ቁጥር ለመቀየር ይንኩ';
+
+  @override
+  String get all => 'ሁሉም';
+
+  @override
+  String get psalms => 'መዝሙረ ዳዊት';
+
+  @override
+  String get splashTitle => 'ሳባ ቅዱስ ስነ-ጽሁፍ';
+
+  @override
+  String get splashSubtitle => 'SABA SACRED SCRIPT';
+
+  @override
+  String get splashTagline => 'የቃሉ ብርሃን';
+}
