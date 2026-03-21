bcf2fa68306b1d1c0f8e2125e798401a4f5e9009
Nathnael Nigussie <nathnaelnigussie19@gmail.com>
2026-03-21 22:47:07 +0300

    Update IReadingPlanRepository to support localization by adding optional locale parameters to getReadingPlans and getReadingPlanById methods. This change enhances the flexibility of the repository for handling multiple languages.

--- lib/features/reading_plans/domain/repositories/i_reading_plan_repository.dart
+++ lib/features/reading_plans/domain/repositories/i_reading_plan_repository.dart
@@ -1,8 +1,8 @@
 import '../entities/reading_plan.dart';
 
 abstract class IReadingPlanRepository {
-  Future<List<ReadingPlan>> getReadingPlans();
-  Future<ReadingPlan?> getReadingPlanById(String id);
+  Future<List<ReadingPlan>> getReadingPlans({String locale = 'en'});
+  Future<ReadingPlan?> getReadingPlanById(String id, {String locale = 'en'});
   Future<List<String>> getCompletedTaskIds(String planId);
   Future<void> toggleTaskCompletion(String planId, String taskId);
   Future<void> clearPlanProgress(String planId);
